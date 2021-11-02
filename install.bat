d8c794b8d2b064ec8f112804a97a117715517c9a
NikolayKaurov <nikolaykaurov@gmail.com>
2021-11-02 11:55:48 +0700

    Проверил всё, кроме дропдауна и календаря

--- install.bat
+++ install.bat
@@ -1,5 +1,6 @@
 call npm init -y
-call npm install --save-dev uuid acorn webpack webpack-cli webpack-dev-server pug simple-pug-loader css-loader style-loader sass sass-loader html-webpack-plugin mini-css-extract-plugin resolve-url-loader terser-webpack-plugin jquery jsdom eslint npm-add-script
-call npx npmAddScript -k start -v "webpack serve --open"
-call npx npmAddScript -k build -v "webpack"
+call npm install --save-dev uuid acorn webpack webpack-cli webpack-dev-server pug simple-pug-loader css-loader style-loader sass sass-loader html-webpack-plugin mini-css-extract-plugin resolve-url-loader terser-webpack-plugin jquery jsdom eslint
+call npm install --global npm-add-script
+call npmAddScript -k start -v "webpack serve --open"
+call npmAddScript -k build -v "webpack"
 call echo FINISH
