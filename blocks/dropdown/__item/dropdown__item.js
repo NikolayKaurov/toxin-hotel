f88fb6baa14f65d9d087296cf4d604c339cd62da
NikolayKaurov <nikolaykaurov@gmail.com>
2021-12-01 17:48:57 +0700

    Дропдаун в процессе

--- blocks/dropdown/__item/dropdown__item.js
+++ blocks/dropdown/__item/dropdown__item.js
@@ -3,6 +3,79 @@ import $ from 'jquery';
 // максимальное значение счётчика, минимальное значение 0
 const MAX = 99;
 
+function handleCounterButtonMousedown(event) {
+  event.data.$dropdown.addClass('dropdown_keeping-focus');
+
+  $(event.target).addClass('js-dropdown__counter-button_pressed');
+
+  if ($(event.target).hasClass('js-dropdown__counter-button_action_plus')) {
+    event.data.$dropdown__quantity.val(parseInt(event.data.$dropdown__quantity.val(), 10) + 1);
+  } else {
+    event.data.$dropdown__quantity.val(parseInt(event.data.$dropdown__quantity.val(), 10) - 1);
+  }
+
+  event.data.$dropdown__quantity.trigger('input');
+}
+
+function handleCounterButtonMouseup(event) {
+  if ($(event.target).hasClass('js-dropdown__counter-button_pressed')) {
+    $(event.target).removeClass('js-dropdown__counter-button_pressed');
+
+    event.data.$dropdown.trigger('focus');
+  }
+}
+
+function handleQuantityInput(event) {
+  const value = parseInt($(event.target).val(), 10);
+
+  event.data.dropdown__item.$dropdown__counterButtons.each((index, element) => {
+    if ($(element).hasClass('js-dropdown__counter-button_action_plus')) {
+      if (value < MAX) {
+        $(element).prop('disabled', false);
+      } else {
+        $(element).prop('disabled', true);
+      }
+    } else if (value > 0) {
+      $(element).prop('disabled', false);
+    } else {
+      $(element).prop('disabled', true);
+    }
+  });
+
+  if (value === 0) {
+    event.data.dropdown__item.$dropdown__item.attr('data-value', '');
+  } else if ((value > 4 && value < 21) || value % 10 > 4 || value % 10 === 0) {
+    event.data.dropdown__item.$dropdown__item.attr(
+      'data-value',
+      `${value} ${event.data.dropdown__item.unitsGenitivePlural}`,
+    );
+  } else if (value % 10 === 1) {
+    event.data.dropdown__item.$dropdown__item.attr(
+      'data-value',
+      `${value} ${event.data.dropdown__item.unitsNominative}`,
+    );
+  } else {
+    event.data.dropdown__item.$dropdown__item.attr(
+      'data-value',
+      `${value} ${event.data.dropdown__item.unitsGenitive}`,
+    );
+  }
+}
+/*
+
+function handlePlusMousedown(event) {
+  event.data.$dropdown.addClass('dropdown_keeping-focus');
+
+  event.data.$dropdown__quantity.val(parseInt(event.data.$dropdown__quantity.val(), 10) + 1);
+}
+
+function handleMinusMousedown(event) {
+  event.data.$dropdown.addClass('dropdown_keeping-focus');
+
+  event.data.$dropdown__quantity.val(parseInt(event.data.$dropdown__quantity.val(), 10) - 1);
+}
+*/
+
 /* eslint-disable-next-line */
 class Dropdown__item {
   constructor(item) {
@@ -13,7 +86,6 @@ class Dropdown__item {
     this.unitsNominative = undefined;
     this.unitsGenitive = undefined;
     this.unitsGenitivePlural = undefined;
-
     [this.units, this.unitsNominative, this.unitsGenitive, this.unitsGenitivePlural] = item.dataset.units.split(' ');
     if (this.unitsNominative === undefined) {
       this.unitsNominative = this.units;
@@ -27,13 +99,39 @@ class Dropdown__item {
 
     this.$dropdown = $(`.js-dropdown[data-dropdown-name="${item.dataset.dropdownName}"]`);
     this.$dropdown__label = $('.js-dropdown__label', this.$dropdown__item);
-    this.$dropdown__counterButton_actionPlus = $('.js-dropdown__counter-button_action_plus', this.$dropdown__item);
-    this.$dropdown__counterButton_actionMinus = $('.js-dropdown__counter-button_action_minus', this.$dropdown__item);
+    this.$dropdown__counterButtons = $('.js-dropdown__counter-button', this.$dropdown__item);
     this.$dropdown__quantity = $('.js-dropdown__quantity', this.$dropdown__item);
   }
 
   init() {
+    this.$dropdown__item.attr('data-value', '');
+
     this.$dropdown__label.text(this.units);
+    this.$dropdown__quantity.val(0);
+
+    this.$dropdown__counterButtons.on(
+      `mousedown.dropdown__counter-button.${this.name}`,
+      null,
+      {
+        $dropdown: this.$dropdown,
+        $dropdown__quantity: this.$dropdown__quantity,
+      },
+      handleCounterButtonMousedown,
+    );
+
+    this.$dropdown__counterButtons.on(
+      `mouseup.dropdown__counter-button.${this.name} mouseout.dropdown__counter-button.${this.name}`,
+      null,
+      { $dropdown: this.$dropdown },
+      handleCounterButtonMouseup,
+    );
+
+    this.$dropdown__quantity.on(
+      `input.dropdown__quantity.${this.name}`,
+      null,
+      { dropdown__item: this },
+      handleQuantityInput,
+    );
   }
 }
 
--- blocks/dropdown/dropdown.js
+++ blocks/dropdown/dropdown.js
@@ -15,103 +15,92 @@ const BUTTON_CONTAINER_HEIGHT = 41;
 // Высота выпадающего элемента в открытом состоянии без строк в пикселах
 const EMPTY_OPEN_HEIGHT = 52;
 
+function handleFocus(event) {
+  event.data.dropdown.$dropdown.addClass('dropdown_open');
+  event.data.dropdown.$dropdown__down.css('height', event.data.dropdown.openHeight);
+  event.data.dropdown.setTimeFocus(event.timeStamp);
+}
+
+function handleBlur(event) {
+  if (event.data.dropdown.$dropdown.hasClass('dropdown_keeping-focus')) {
+    event.data.dropdown.$dropdown.removeClass('dropdown_keeping-focus');
+    return;
+  }
+
+  event.data.dropdown.$dropdown.removeClass('dropdown_open');
+  event.data.dropdown.$dropdown__down.css('height', event.data.dropdown.closedHeight);
+}
+
+function handleDropMousedown(event) {
+  // console.log(`ИНТЕРВАЛ: ${event.timeStamp - event.data.dropdown.timeFocus}`);
+  if (Math.abs(event.timeStamp - event.data.dropdown.timeFocus) < INTERVAL) {
+    return;
+  }
+
+  if (event.data.dropdown.$dropdown.hasClass('dropdown_open')) {
+    event.data.dropdown.$dropdown.removeClass('dropdown_open');
+    event.data.dropdown.$dropdown__down.css('height', event.data.dropdown.closedHeight);
+  } else {
+    event.data.dropdown.$dropdown.addClass('dropdown_open');
+    event.data.dropdown.$dropdown__down.css('height', event.data.dropdown.openHeight);
+  }
+}
+
 class Dropdown {
   constructor(dropdown) {
     this.$dropdown = $(dropdown);
 
     this.name = dropdown.dataset.dropdownName;
-
     this.zIndex = dropdown.dataset.zIndex;
+    this.defaultValue = dropdown.dataset.defaultValue;
 
-    // this.openHeight = EMPTY_OPEN_HEIGHT;
+    this.openHeight = '';
+    this.closedHeight = '';
 
-    this.$dropdown__items = $('.js-dropdown__item', this.$dropdown);
-    /*
-    this.$dropdown__items.each((index, item) => {
-      $(item).attr('data-dropdown-name', this.name);
-
-      this.openHeight += ITEM_HEIGHT;
-    });
-    */
-
-    /*
-    if ($('.js-dropdown__button-container', this.$dropdown).length) {
-      this.openHeight += BUTTON_CONTAINER_HEIGHT;
-    }
-    this.openHeight += 'px';
-    */
-
-    this.$dropdown__down = $('.js-dropdown__down', this.$dropdown);
-    /*
-    this.$dropdown__down.css({
-      height: CLOSED_HEIGHT,
-      'z-index': () => 2 * this.zIndex - 1,
-    });
-    */
-
+    this.$dropdown__value = $('.js-dropdown__value', this.$dropdown);
     this.$dropdown__drop = $('.js-dropdown__drop', this.$dropdown);
-    /*
-    this.$dropdown__drop.css({
-      'z-index': () => 2 * this.zIndex,
-    });
-    */
+    this.$dropdown__down = $('.js-dropdown__down', this.$dropdown);
+    this.$dropdown__items = $($('.js-dropdown__item', this.$dropdown).get().reverse());
+    this.$dropdown__confirm = $('.js-dropdown__confirm', this.$dropdown);
 
-    this.time = {
-      timeFocus: 0,
-    };
-
-    this.$dropdown.on('focus.dropdown.' + this.name,
-      null,
-      {$dropdown__down: this.$dropdown__down,
-        openHeight: this.openHeight,
-        time: this.time},
-      handleFocus);
-    function handleFocus( event ) {
-      $(event.target).addClass('dropdown_open');
-      event.data.$dropdown__down.css('height', event.data.openHeight);
-
-      event.data.time.timeFocus = Date.now();
-    }
-
-    this.$dropdown.on('blur.dropdown.' + this.name,
-      null,
-      {$dropdown__down: this.$dropdown__down},
-      handleBlur);
-    function handleBlur( event ) {
-      console.log('BLUR');
-
-      if ($(event.target).hasClass('dropdown_keeping-focus')) {
-        $(event.target).removeClass('dropdown_keeping-focus');
-        return;
-      }
-
-      $(event.target).removeClass('dropdown_open');
-      event.data.$dropdown__down.css('height', CLOSED_HEIGHT);
-    }
-
-    this.$dropdown__drop.on('mousedown.dropdown__drop.' + this.name,
-      null,
-      {$dropdown: this.$dropdown,
-        $dropdown__down: this.$dropdown__down,
-        time: this.time,
-        openHeight: this.openHeight},
-      handleDropMousedown);
-    function handleDropMousedown( event ) {
-      if (Date.now() - event.data.time.timeFocus < INTERVAL) {
-        return;
-      }
-
-      if (event.data.$dropdown.hasClass('dropdown_open')) {
-        event.data.$dropdown.removeClass('dropdown_open');
-        event.data.$dropdown__down.css('height', CLOSED_HEIGHT);
-      } else {
-        event.data.$dropdown.addClass('dropdown_open');
-        event.data.$dropdown__down.css('height', event.data.openHeight);
-      }
-    }
+    this.timeFocus = 0;
   }
 
   init() {
+    let openHeight = EMPTY_OPEN_HEIGHT;
+
+    this.$dropdown__items.each((index, item) => {
+      $(item).attr('data-dropdown-name', this.name);
+
+      openHeight += ITEM_HEIGHT;
+    });
+
+    if ($('.js-dropdown__button-container', this.$dropdown).length) {
+      openHeight += BUTTON_CONTAINER_HEIGHT;
+    }
+
+    this.openHeight = `${openHeight}px`;
+    this.closedHeight = `${CLOSED_HEIGHT}px`;
+
+    this.$dropdown__down.css({
+      height: this.closedHeight,
+      'z-index': () => 2 * this.zIndex - 1,
+    });
+
+    this.$dropdown__drop.css({
+      'z-index': () => 2 * this.zIndex,
+    });
+
+    this.$dropdown__value.text(this.defaultValue);
+
+    this.$dropdown.on(`focus.dropdown.${this.name}`, null, { dropdown: this }, handleFocus);
+    this.$dropdown.on(`blur.dropdown.${this.name}`, null, { dropdown: this }, handleBlur);
+
+    this.$dropdown__drop.on(`mousedown.dropdown__drop.${this.name}`, null, { dropdown: this }, handleDropMousedown);
+  }
+
+  setTimeFocus(time) {
+    this.timeFocus = time;
   }
 }
 
