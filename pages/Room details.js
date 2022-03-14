021e39f06a9d3a677a30155564ddfe3da4b45f46
NikolayKaurov <nikolaykaurov@gmail.com>
2022-03-14 18:02:53 +0700

    Диаграмма в процессе

--- pages/Room details.js
+++ pages/Room details.js
@@ -3,6 +3,7 @@ import '../blocks/card-details/card-details';
 import '../blocks/datepicker/datepicker';
 import '../blocks/dropdown/dropdown';
 import '../blocks/dropdown/_guests/dropdown_guests';
+import '../blocks/like/like';
 import '../blocks/subscription/subscription';
 import '../blocks/dropdown/__button/dropdown__button';
 import '../blocks/dropdown/__item/dropdown__item';
