320f48461c8481034c430a06f01feb209a6824ae
NikolayKaurov <nikolaykaurov@gmail.com>
2022-04-28 21:12:00 +0700

    updating text-field

--- blocks/text-field/text-field.js
+++ blocks/text-field/text-field.js
@@ -10,112 +10,165 @@ function getPlaceholder(value) {
   return placeholder.join('');
 }
 
-/*
-function handleInputKeydown(event) {
-  const value = $(event.target).val();
+function isCorrectDate(days, month, year) {
+  let daysTemp;
 
-  if (
-    (
-      event.key < '0' || event.key > '9'
-      || value.match(/^\d{2}\.\d{2}\.\d{4}$/)
-      || (value === '0' && event.key === '0')
-      || (value.match(/^\d{2}.0$/) && event.key === '0')
-    )
-    && event.key !== 'Backspace'
-    && event.key !== 'Tab'
-  ) {
-    event.preventDefault();
-    return;
+  if (days) {
+    daysTemp = parseInt(days, 10);
+  } else {
+    daysTemp = 0;
   }
 
-  if (
-    (value.match(/^\d{2}\.\d$/))
-    && (event.key === 'Backspace')
-  ) {
-    $(event.target).val(value.replace(/\./, ''));
-  } else if (
-    (value.match(/^\d{2}\.\d{2}\.\d$/))
-    && (event.key === 'Backspace')
-  ) {
-    $(event.target).val(value.replace(/\.(?=\d$)/, ''));
-  } else if (
-    (value === '')
-    && (event.key >= '4' && event.key <= '9')
-  ) {
-    $(event.target).val('0');
-  } else if (
-    (value === '3')
-    && (event.key >= '2' && event.key <= '9')
-  ) {
-    $(event.target).val('03.0');
-  } else if (
-    (value.match(/^\d{2}$/))
-    && (event.key >= '2' && event.key <= '9')
-  ) {
-    $(event.target).val(`${value}.0`);
-  } else if (
-    (value.match(/^\d{2}$/))
-    && (event.key >= '0' && event.key <= '1')
-  ) {
-    $(event.target).val(`${value}.`);
-  } else if (
-    (value.match(/^\d{2}\.1$/))
-    && (event.key >= '3' && event.key <= '9')
-  ) {
-    $(event.target).val(`${value.replace(/\.1$/, '.01.19')}`);
-  } else if (
-    (value.match(/^\d{2}\.\d{2}$/))
-    && (event.key >= '1' && event.key <= '2')
-  ) {
-    $(event.target).val(`${value}.`);
-  } else if (
-    (value.match(/^\d{2}\.\d{2}$/))
-    && (event.key >= '3' && event.key <= '9')
-  ) {
-    $(event.target).val(`${value}.19`);
-  } else if (
-    (value.match(/^\d{2}\.\d{2}$/))
-    && (event.key === '0')
-  ) {
-    $(event.target).val(`${value}.20`);
-  } else if (
-    (value.match(/^\d{2}\.\d{2}\.1$/))
-    && (event.key >= '0' && event.key <= '8')
-  ) {
-    $(event.target).val(`${value.replace(/\.1$/, '.201')}`);
-  } else if (
-    (value.match(/^\d{2}\.\d{2}\.2$/))
-    && (event.key >= '1' && event.key <= '9')
-  ) {
-    $(event.target).val(`${value.replace(/\.2$/, '.202')}`);
+  let wrong = (days === '00') || daysTemp > 31;
+
+  if (wrong) {
+    return false;
   }
+
+  let monthTemp;
+
+  if (month) {
+    monthTemp = parseInt(month, 10);
+  } else {
+    monthTemp = 0;
+  }
+
+  wrong = (month === '00') || monthTemp > 12;
+
+  if (wrong) {
+    return false;
+  }
+
+  const yearTooShort = !year
+    || (year.length < 4 && year.match(/^(19|20)/))
+    || year.length === 1
+    || year.length === 3;
+
+  let yearTemp;
+
+  if (yearTooShort) {
+    yearTemp = 0;
+  } else if (year.length === 2) {
+    if (parseInt(year, 10) < 30) {
+      yearTemp = parseInt(`20${year}`, 10);
+    } else {
+      yearTemp = parseInt(`19${year}`, 10);
+    }
+  } else {
+    yearTemp = parseInt(year, 10);
+  }
+
+  wrong = ((daysTemp > 29) && (monthTemp === 2))
+    || ((daysTemp > 28) && (monthTemp === 2) && (yearTemp % 4 !== 0 || yearTemp === 1900));
+
+  if (wrong) {
+    return false;
+  }
+
+  wrong = (daysTemp === 31) && [4, 6, 9, 10].includes(monthTemp);
+
+  return !wrong;
 }
-*/
 
 function handleInputPaste(event) {
   event.preventDefault();
 }
 
 function handleInputInput(event) {
+  const input = event.target;
   const $input = $(event.target);
+
+  let selectPos = input.selectionStart;
 
   let value = $input.val();
 
   const { preValue, $double } = event.data.textField;
 
   if (preValue.length > value.length) {
-    const dotHasBeenRemoved = preValue === `${value}.`;
+    if (!value.match(/^(\d{0,2}\.?|\d{0,2}\.\d{0,2}\.?|\d{0,2}\.\d{0,2}\.\d{0,4})$/)) {
+      value = preValue;
+    } else {
+      value = value.replace(/\.$/, ''); // удалить точки в конце строки, если одна - одну
+      value = value.replace(/\.$/, ''); // если 2 - обе
+    }
+    /* const dotHasBeenRemoved = preValue === `${value}.`;
 
     const numberAfterDotHasBeenRemoved = value.match(/\.$/)
       && value === preValue.slice(0, preValue.length - 1);
-    /* if (value.match(/^(\d{2}|\d{2}\.\d{2})$/)) {
-      value = value.replace(/\d$/, '');
-    }
-    value = value.replace(/\.$/, ''); */
+
     if (dotHasBeenRemoved || numberAfterDotHasBeenRemoved) {
       value = value.slice(0, value.length - 1);
+    } */
+  } else if (!value.match(/^(\d{0,3}|\d{0,2}\.\d{1,3}|\d{0,2}\.\d{0,2}\.\d{1,4})$/)) {
+    value = preValue;
+    selectPos -= 1;
+  } else {
+    /* if (value.match(/^(\d{3}|\d{0,2}\.\d{3})$/)) {
+      if (selectPos === value.length) {
+        selectPos += 1;
+      }
+      value = `${value.slice(0, value.length - 1)}.${value[value.length - 1]}`;
+    } */
+    let days;
+    let month;
+    let year;
+
+    if (value.match(/^\d{3}$/)) {
+      days = value.slice(0, 2);
+      month = value.slice(2);
+    } else if (value.match(/^\d{0,2}\.\d{3}$/)) {
+      [days, month] = value.slice(0, value.length - 1).split('.');
+      year = value[value.length - 1];
+    } else {
+      [days, month, year] = value.split('.');
     }
-  } else if (value.match(/^[4-9]$/)) {
+
+    if (isCorrectDate(days, month, year)) {
+      if (days) {
+        if (days.match(/^[4-9]$/)) {
+          days = `0${days}`;
+        }
+      }
+
+      if (month) {
+        if (month.match(/^[2-9]$/)) {
+          month = `0${month}`;
+        }
+      }
+
+      if (year) {
+        const twoDigitYear = (year.length === 2) && (!year.match(/^(19|20)$/));
+        if (twoDigitYear) {
+          if (parseInt(year, 10) < 30) {
+            year = parseInt(`20${year}`, 10);
+          } else {
+            year = parseInt(`19${year}`, 10);
+          }
+        }
+      }
+
+      const newValue = [];
+
+      if (days !== undefined) {
+        newValue.push(days);
+      }
+
+      if (month !== undefined) {
+        newValue.push(month);
+      }
+
+      if (year !== undefined) {
+        newValue.push(year);
+      }
+
+      value = newValue.join('.');
+    } else {
+      value = preValue;
+      selectPos -= 1;
+    }
+    // console.log([days, month, year].join('.'));
+  }
+  /* else if (value.match(/^[4-9]$/)) {
     value = `0${value}.`;
   } else if (value.match(/^(0[1-9]|[12]\d|3[01])$/)) {
     value = `${value}.`;
@@ -151,12 +204,14 @@ function handleInputInput(event) {
     value = `${value.slice(0, 5)}.${value[5]}`;
   } else if (value.match(/^\d{2}\.\d{2}\.(2[1-9]|1[0-8])$/)) {
     value = `${value.slice(0, 6)}20${value[6]}${value[7]}`;
-  } else if (!value.match(/^(\d{1,2}|\d{2}\.\d{0,2}|\d{2}\.\d{2}\.\d{0,4})$/)) {
+  } */ /* else if (!value.match(/^(\d{1,2}|\d{2}\.\d{0,2}|\d{2}\.\d{2}\.\d{0,4})$/)) {
     value = preValue;
-  }
+  } */
 
   $input.val(value);
   $double.attr('placeholder', getPlaceholder(value));
+
+  input.setSelectionRange(selectPos, selectPos);
 
   event.data.textField.setPreValue(value);
 
