39a251456814f96316cfe6c8c89c9e300e2eaaac
NikolayKaurov <nikolaykaurov@gmail.com>
2022-05-29 00:12:52 +0700

    redundant files removed

--- pages/Registration.js
+++
@@ -1,6 +0,0 @@
-import '../blocks/burger/burger';
-import '../blocks/card-registration/card-registration';
-import '../blocks/text-field/text-field';
-import '../blocks/subscription/subscription';
-import './registration.scss';
-import '../favicons/favicons';
