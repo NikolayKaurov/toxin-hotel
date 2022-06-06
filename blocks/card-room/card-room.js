e61e74746647e53c3ae78606148f1f5acdca1ce5
NikolayKaurov <nikolaykaurov@gmail.com>
2022-06-06 17:46:16 +0700

    fixed card-room UX#1

--- blocks/card-room/card-room.js
+++ blocks/card-room/card-room.js
@@ -1,44 +1,29 @@
 import $ from 'jquery';
 
-function handleBackMousedown(event) {
-  const { $card } = event.data;
-
-  let slide = parseInt($card.attr('data-slide'), 10);
-  slide -= 1;
-  if (slide < 0) {
-    slide = 3;
-  }
-  $card.attr('data-slide', slide);
-}
-
-function handleForwardMousedown(event) {
-  const { $card } = event.data;
-
-  let slide = parseInt($card.attr('data-slide'), 10);
-  slide += 1;
-  if (slide > 3) {
-    slide = 0;
-  }
-  $card.attr('data-slide', slide);
-}
-
-function handleNavMousedown(event) {
-  event.data.$card.attr('data-slide', parseInt(event.target.dataset.slide, 10));
-}
-
 class CardRoom {
   #$card;
 
+  #$form;
+
+  #slide;
+
   constructor(card) {
     this.#$card = $(card);
+
+    this.#slide = 0;
   }
 
   init() {
+    this.#$card
+      .on('keydown', { card: this }, handleCardKeydown);
+
+    this.#$form = $('.js-card-room__form', this.#$card);
+
     $('.js-card-room__back', this.#$card)
       .on(
         'mousedown',
         null,
-        { $card: this.#$card },
+        { card: this },
         handleBackMousedown,
       );
 
@@ -46,7 +31,7 @@ class CardRoom {
       .on(
         'mousedown',
         null,
-        { $card: this.#$card },
+        { card: this },
         handleForwardMousedown,
       );
 
@@ -54,13 +39,89 @@ class CardRoom {
       .on(
         'mousedown',
         '.js-card-room__nav-item',
-        { $card: this.#$card },
+        { card: this },
         handleNavMousedown,
       );
   }
+
+  back() {
+    this.#slide -= 1;
+
+    if (this.#slide < 0) {
+      this.#slide = 3;
+    }
+
+    this.#$card.attr('data-slide', this.#slide);
+  }
+
+  forward() {
+    this.#slide += 1;
+
+    if (this.#slide > 3) {
+      this.#slide = 0;
+    }
+
+    this.#$card.attr('data-slide', this.#slide);
+  }
+
+  setSlide(slide) {
+    this.#slide = slide;
+    this.#$card.attr('data-slide', slide);
+  }
+
+  confirm() {
+    this.#$form.trigger('submit');
+  }
 }
 
-$('.js-card-room').each((index, element) => {
-  const card = new CardRoom(element);
-  card.init();
+function handleCardKeydown(event) {
+  const { keyCode } = event;
+  const { card } = event.data;
+
+  if (keyCode === 32 || keyCode === 13) {
+    event.preventDefault();
+
+    card.confirm();
+
+    return;
+  }
+
+  if (keyCode === 37) {
+    event.preventDefault();
+
+    card.back();
+
+    return;
+  }
+
+  if (keyCode === 39) {
+    event.preventDefault();
+
+    card.forward();
+  }
+}
+
+function handleBackMousedown(event) {
+  const { card } = event.data;
+
+  card.back();
+}
+
+function handleForwardMousedown(event) {
+  const { card } = event.data;
+
+  card.forward();
+}
+
+function handleNavMousedown(event) {
+  const { card } = event.data;
+
+  const slide = parseInt(event.target.dataset.slide, 10);
+
+  card.setSlide(slide);
+}
+
+$('.js-card-room').each((index, card) => {
+  const jsCard = new CardRoom(card);
+  jsCard.init();
 });
