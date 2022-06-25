94dc537513b85c15b420d99a14607745f342fa0c
NikolayKaurov <nikolaykaurov@gmail.com>
2022-06-25 16:29:53 +0700

    fixed id in radio and checkbox HTML #1

--- blocks/checkbox/checkbox.js
+++ blocks/checkbox/checkbox.js
@@ -1,6 +1,14 @@
 import $ from 'jquery';
+import { v4 as getId } from 'uuid';
 
 $(document).on('keydown', '.checkbox__input', handleCheckKeydown);
+
+$('.js-checkbox__input').each((index, checkbox) => {
+  const id = getId();
+  $(checkbox)
+    .attr('id', id)
+    .next().attr('for', id);
+});
 
 function handleCheckKeydown(event) {
   if (event.keyCode === 13 || event.keyCode === 32) {
